a4899749edd9379d5db2d0e71e348260cb6bec20
adam-lafontaine <adam.lafontaine@gmail.com>
2020-08-19 21:12:39 -0400

    windows client, linux server

--- Windows/cpp/launcher_main.cpp
+++ Windows/cpp/launcher_main.cpp
@@ -1,6 +1,9 @@
-#include "../hpp/launchers/client_server_threaded.hpp"
+//#include "../hpp/launchers/client_server_threaded.hpp"
+#include "../hpp/launchers/client_launcher.hpp"
 
 int main(int argc, char *argv[])
 {
-	client_server_threaded();
+	//client_server_threaded();
+
+	launch_client(argc, argv);
 }
