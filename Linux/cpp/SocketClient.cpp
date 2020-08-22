427c82a82ed8a9961d49bd4d48b3f3ca59048589
adam-lafontaine <adam.lafontaine@gmail.com>
2020-08-22 17:37:12 -0400

    remove includes

--- Linux/cpp/SocketClient.cpp
+++ Linux/cpp/SocketClient.cpp
@@ -1,14 +1,8 @@
 #include "../hpp/SocketClient.hpp"
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <arpa/inet.h>
+
 #include <sstream>
 #include <cassert>
 
--- Linux/cpp/SocketServer.cpp
+++ Linux/cpp/SocketServer.cpp
@@ -1,15 +1,10 @@
 #include "../hpp/SocketServer.hpp"
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <netdb.h>
-#include<arpa/inet.h>
 #include<ifaddrs.h>
+
 #include<sstream>
 #include<cassert>
 
