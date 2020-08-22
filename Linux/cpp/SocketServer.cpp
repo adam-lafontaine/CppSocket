3298e7f39ae648a6da1e97ff837cb1d3dad9d1c6
adam-lafontaine <adam.lafontaine@gmail.com>
2020-08-22 11:44:44 -0400

    move server networking code

--- Linux/cpp/SocketServer.cpp
+++ Linux/cpp/SocketServer.cpp
@@ -1,49 +1,93 @@
 #include "../hpp/SocketServer.hpp"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include<arpa/inet.h>
+#include<ifaddrs.h>
+#include<sstream>
+#include<cassert>
+
+static std::string to_csv(std::vector<std::string> const& list)
+{
+    const auto delim = ", ";
+
+    std::string msg = "";
+    for (auto const& err : list)
+    {
+        msg += err;
+        msg += delim;
+    }
+
+    msg.pop_back();
+    msg.pop_back();
+
+    return msg;
+}
+
 namespace SocketLib
 {
-    std::string SocketServer::system_error(std::string const& msg)
+    struct ServerSocketInfo
+    {
+        struct sockaddr_in srv_addr = { 0 };
+        struct sockaddr_in cli_addr = { 0 };
+
+        int srv_socket;
+        int cli_socket;
+        
+        socklen_t cli_len;
+
+        bool srv_running = false;
+		bool cli_connected = false;
+    };
+    
+
+    static void create_server_socket(ServerSocketInfo* info, unsigned short port)
+    {
+        info->srv_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        info->srv_addr = { 0 };
+		info->srv_addr.sin_family = AF_INET;
+        info->srv_addr.sin_addr.s_addr = INADDR_ANY;
+        info->srv_addr.sin_port = htons(port);
+    }
+
+
+    void SocketServer::create_socket_info()
+    {
+        if(m_socket_info == nullptr)
+			m_socket_info = new socket_info_t;
+
+		create_server_socket(m_socket_info, m_port_no);
+    }
+
+
+	void SocketServer::destroy_socket_info()
+    {
+        if (m_socket_info == nullptr)
+			return;
+    }
+
+
+    bool SocketServer::running()
 	{
-		return msg + ": " + strerror(errno);
+		return m_socket_info != nullptr && m_socket_info->srv_running;
 	}
 
 
-    std::string SocketServer::to_csv(std::vector<std::string> const& list)
+	bool SocketServer::connected()
 	{
-		const auto delim = ", ";
-
-		std::string msg = "";
-		for (auto const& err : list)
-		{
-			msg += err;
-			msg += delim;
-		}
-
-		msg.pop_back();
-		msg.pop_back();
-
-		return msg;
+		return m_socket_info != nullptr && m_socket_info->cli_connected;
 	}
 
 
     bool SocketServer::init() 
     {
-        m_srv_socket = socket(AF_INET, SOCK_STREAM, 0); // create socket
-
-        if (m_srv_socket < 0)
-        {
-            m_status = "ERROR opening socket";
-            m_errors.push_back(system_error(m_status));
-            return false;
-        }
-
-        m_open = true;
-
-        bzero((char *)&m_srv_addr, sizeof(m_srv_addr)); // initialize serv_addr to zeros        
-
-        m_srv_addr.sin_family = AF_INET;          // always
-        m_srv_addr.sin_addr.s_addr = INADDR_ANY;  // IP address of server machine
-        m_srv_addr.sin_port = htons(m_port_no);    // convert from host to network byte order
+        create_socket_info();
 
         m_status = "Server Initialized";
         return true;
@@ -51,13 +95,14 @@ namespace SocketLib
 
     bool SocketServer::bind_socket()
     {
-        // bind socket to server address
-        const int bind_res = bind(m_srv_socket, (struct sockaddr*) &m_srv_addr, sizeof(m_srv_addr));
+        auto socket = m_socket_info->srv_socket;
+        auto addr = (struct sockaddr*)&m_socket_info->srv_addr;
+        auto size = sizeof(m_socket_info->srv_addr);
 
-        if (bind_res < 0)
+        if (bind(socket, addr, size) < 0)
         {
             m_status = "ERROR binding socket";
-            m_errors.push_back(system_error(m_status));
+            m_errors.push_back(m_status);
             return false;
         }
 
@@ -68,12 +113,13 @@ namespace SocketLib
 
     bool SocketServer::listen_socket()
     {
-        const int res = listen(m_srv_socket, 5);
+        m_socket_info->srv_running = false;
         const auto port = std::to_string(m_port_no);
-        if(res < 0)
+        
+        if(listen(m_socket_info->srv_socket, 5) < 0)
         {
             m_status = "ERROR listening on port " + port;
-            m_errors.push_back(system_error(m_status));
+            m_errors.push_back(m_status);
             return false;
         }
 
@@ -81,19 +127,58 @@ namespace SocketLib
         return true;
     }
 
-    void SocketServer::close_socket()
-    {
-        if(!m_open)
-            return;
 
-        close(m_srv_socket);
-        
-        m_open = false;
+    bool SocketServer::connect_client()
+    {
+        if (!m_socket_info->srv_running)
+		{
+			m_status = "Server cannot connect, server not running";
+			m_errors.push_back(m_status);
+			return false;
+		}
+
+        m_socket_info->cli_len = sizeof(m_socket_info->cli_addr);
+
+        m_status = "Server waiting for client";
+		m_socket_info->cli_connected = false;
+
+        auto srv_socket = m_socket_info->srv_socket;
+        auto cli_addr = (struct sockaddr *)&m_socket_info->cli_addr;
+        auto cli_len = &m_socket_info->cli_len;
+
+        // waits for client to connect        
+        m_socket_info->cli_socket = accept(srv_socket, cli_addr, cli_len);
+
+        if (m_socket_info->cli_socket < 0)
+        {
+            m_status = "ERROR on accept";
+            m_errors.push_back(m_status);
+            return false;
+        }
+
+        m_status = "Server connected to client";
+		m_socket_info->cli_connected = true;
+
+		return true;
     }
+
+    void SocketServer::disconnect_client()
+    {
+		if (!connected())
+			return;
+        
+        close(m_socket_info->cli_socket);
+
+        m_status = "Server disonnected from client";
+
+        m_socket_info->cli_connected = false;
+	}
+
 
     void SocketServer::start()
     {
-        m_running = false;
+        if(running())
+			m_socket_info->srv_running = false;
 
 		if (!init() || !bind_socket() || !listen_socket())
         {
@@ -101,75 +186,42 @@ namespace SocketLib
             return;
         }
 
-		m_running = true;
+		m_socket_info->srv_running = true;
     }
 
     void SocketServer::stop()
     {
 		disconnect_client();
-
-		m_running = false;
+        
 		close_socket();
 		m_status = "Server stopped";
 	}
 
-    bool SocketServer::connect_client()
+
+
+    void SocketServer::close_socket()
     {
-        if (!m_running)
-        {
-			m_status = "Server cannot connect, server not running";
-			return false;
-		}
-
-        m_cli_len = sizeof(m_cli_addr);
-
-        m_status = "Server waiting for client";
-		m_connected = false;
-
-        // waits for client to connect        
-        m_cli_socket = accept(m_srv_socket, (struct sockaddr *) &m_cli_addr, &m_cli_len);
-
-        if (m_cli_socket < 0)
-        {
-            m_status = "ERROR on accept";
-            m_errors.push_back(system_error(m_status));
-            return false;
-        }
-
-        m_status = "Server connected to client";
-		m_connected = true;
-
-		return true;
-    }
-
-    void SocketServer::disconnect_client()
-    {
-		if (!m_connected)
+        if(!running())
             return;
-        
-        close(m_cli_socket);
 
-        m_status = "Server disonnected from client";
-
-        m_connected = false;
-	}
+        close(m_socket_info->srv_socket);
+        m_socket_info->srv_running = false;
+    }
+    
 
     std::string SocketServer::receive_text()
     {
-		assert(m_running);
-		assert(m_connected);
+		assert(running());
+		assert(connected());
 
-		char buffer[MAX_CHARS]; // characters are read into this buffer
-
-		bzero(buffer, MAX_CHARS); // initialize buffer to zeros
-
-        // waits for message from client to be read to buffer
-        int n_chars = read(m_cli_socket, buffer, MAX_CHARS - 1);
+		char buffer[MAX_CHARS] = "";
+        
+        int n_chars = read(m_socket_info->cli_socket, buffer, MAX_CHARS - 1);
 
         if (n_chars < 0)
         {
             m_status = "ERROR reading from client socket";
-            m_errors.push_back(system_error(m_status));
+            m_errors.push_back(m_status);
             return "error";
         }
 
@@ -179,21 +231,25 @@ namespace SocketLib
 		return oss.str();
 	}
 
+
     bool SocketServer::send_text(std::string const& text)
     {
-		assert(m_running);
-		assert(m_connected);
+		assert(running());
+		assert(connected());
         
-		if (!m_running || !m_connected)
+		if (!running() || !connected())
 			return false;
         
-        // send message to client
-        int n_chars = write(m_cli_socket, text.data(), static_cast<int>(text.size()));
+        auto cli_socket = m_socket_info->cli_socket;
+        auto data = text.data();
+        auto size = static_cast<int>(text.size());
+
+        int n_chars = write(cli_socket, data, size);
 
         if (n_chars < 0)
         {
             m_status = "ERROR writing to client socket";
-            m_errors.push_back(system_error(m_status));
+            m_errors.push_back(m_status);
             return false;
         }
 
--- Linux/hpp/SocketServer.hpp
+++ Linux/hpp/SocketServer.hpp
@@ -1,42 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include<arpa/inet.h>
+#include <string>
+#include <vector>
 
-#include<ifaddrs.h>
-
-#include<vector>
-#include<sstream>
-#include<cassert>
 
 namespace SocketLib 
-{ 
+{
+    struct ServerSocketInfo;
+
     class SocketServer 
     {
-    private:        
+    private:
+
+        using socket_info_t = ServerSocketInfo;
+
+		socket_info_t* m_socket_info = nullptr;
+
         static int constexpr MAX_CHARS = 256;
         unsigned short const DEFAULT_PORT = 27015;
 
-        std::string m_net_interface = "NA";
+        std::string m_net_interface = "NA"; // ?
 
         std::string m_public_ip = "NA";
         unsigned short m_port_no = DEFAULT_PORT;
-
-        int m_srv_socket; // server socket file descriptor
-        int m_cli_socket; // client socket file descriptor
-
-        struct sockaddr_in m_srv_addr = { 0 }; // contains server address
-        struct sockaddr_in m_cli_addr = { 0 };  // contains client address
-        socklen_t m_cli_len; // size of address on client
         
-        bool m_running = false;
-        bool m_open = false;
-        bool m_connected = false;
         std::string m_status = "";
 
         std::vector<std::string> m_errors;
@@ -48,9 +33,8 @@ namespace SocketLib
         bool listen_socket();
         void close_socket();
 
-        // TODO: move to helpers
-		std::string system_error(std::string const& msg);
-		std::string to_csv(std::vector<std::string> const& list);
+        void create_socket_info();
+		void destroy_socket_info();
         
     public:
         SocketServer()
@@ -82,8 +66,8 @@ namespace SocketLib
         bool send_text(std::string const& text);        
 
         std::string status() { return m_status; }
-        bool running() { return m_running; }
-        bool connected() { return m_connected; }
+        bool running();
+		bool connected();
 
         bool has_error() { return !m_errors.empty(); }
         std::string latest_error();
